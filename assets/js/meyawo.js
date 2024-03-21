e79a806ee2d032966e91c941565ea158b217eb6e
Biswajeet Mohanta <biswajeetmohanta123@gmail.com>
2024-03-21 21:45:32 +0530

    update

---
+++ assets/js/meyawo.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// navbar toggle
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+// Progress Bar 
+$(function () {
+  $(".progress").each(function () {
+    var value = $(this).attr("data-value");
+    var left = $(this).find(".progress-left .progress-bar");
+    var right = $(this).find(".progress-right .progress-bar");
+
+    if (value > 0) {
+      if (value <= 50) {
+        right.css("transform", "rotate(" + percentageToDegrees(value) + "deg)");
+      } else {
+        right.css("transform", "rotate(180deg)");
+        left.css(
+          "transform",
+          "rotate(" + percentageToDegrees(value - 50) + "deg)"
+        );
+      }
+    }
+  });
+
+  function percentageToDegrees(percentage) {
+    return (percentage / 100) * 360;
+  }
+});
